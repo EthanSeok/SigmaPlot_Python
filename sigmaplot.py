f017dd59d8e27c0bbb025b5eedecabaa66793238
EthanSeok <champ9162@naver.com>
2023-04-01 14:26:55 +0900

    error bar 추가

--- sigmaplot.py
+++ sigmaplot.py
@@ -24,26 +24,28 @@ def cal(infile, s):
 
     results = differential_evolution(func, bounds, args=args)
     print(f"{s} results:", results.x)
-    return results.x, s
+    return results, s
 
 
 def plot(infile, output_dir, filename):
     fig, ax = plt.subplots(figsize=(9.4,6))
     x = infile['Ca'].sort_values(ascending=True).values
     s_values = ['S1', 'S2', 'S3', 'S4'] if 'S4-1' in infile.columns else ['S1', 'S2', 'S3']
-    print(s_values)
     An_dict = {}
     color_list = ['#7f0000', '#006837', '#feb24c', '#253494']
     for i, (s, label) in enumerate(zip(s_values, ['Ca vs 1-1 - 1-4 \n', 'Ca vs 2-1 - 2-4 \n', 'Ca vs 3-1 - 3-4 \n', 'Ca vs 4-1 - 4-4 \n'] if 'S4-1' in infile.columns else ['Ca vs 1-1 - 1-3 \n', 'Ca vs 2-1 - 2-3 \n', 'Ca vs 3-1 - 3-3 \n'])):
         An = []
-        y0 = cal(infile, s)[0][0]
-        a = cal(infile, s)[0][1]
-        b = cal(infile, s)[0][2]
+        results, s = cal(infile, s)
+        y0 = results.x[0]
+        a = results.x[1]
+        b = results.x[2]
         for j in range(len(x)):
             An.append((y0 + a * x[j] / (b + x[j])))
         An_dict[s] = An
         sns.lineplot(x, An, label=f"{label}: {y0:.3f}+{a:.3f}x/({b:.4f}+x)", marker='o', markersize=8,
                      color=color_list[i])
+        # 에러바 추가
+        plt.errorbar(x, An, yerr=2, fmt='none', ecolor=color_list[i], capsize=3)
 
     ax.set(ylim=(-10, 40))
     ax.set_ylabel('$A(μmol/m^2/s)$')
@@ -52,6 +54,7 @@ def plot(infile, output_dir, filename):
     ax.legend(bbox_to_anchor=(1.02, 1), loc='upper left')
     fig.subplots_adjust(right=0.68)
     fig.savefig(os.path.join(f'{output_dir}/{filename}.png'))
+
 
 
 def main():
